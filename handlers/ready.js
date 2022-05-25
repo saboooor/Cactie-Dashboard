a1f6ee0dc8c58aa4fe9ab03a73f731f567f1e871
Saboor <saboor547@gmail.com>
2022-05-25 00:28:09 -0600

    bruh r u stupiding me

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,6 +1,6 @@
 const Dashboard = require('../dashboard/dashboard.js');
 const { ActivityType } = require('discord.js');
 module.exports = client => client.once('ready', () => {
-	client.user.setPresence({ activities: [{ name: 'Bot down!', type: ActivityType.Game }], status: 'idle' });
+	client.user.setPresence({ activities: [{ name: 'Bot down!', type: ActivityType.Playing }] });
 	Dashboard(client);
 });
