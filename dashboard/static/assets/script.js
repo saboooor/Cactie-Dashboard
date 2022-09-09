6ed1f43ff9b23af44659cf48e75ec8f0d8417434
saboooor <saboor547@gmail.com>
2022-09-08 20:03:59 -0600

    more visuals

--- dashboard/static/assets/script.js
+++ dashboard/static/assets/script.js
@@ -11,42 +11,19 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Functions to open and close a modal
-function openModal($el) {
-	$el.classList.add('is-active');
+function openModal(element, options) {
+	if (options && options.channel) document.getElementsByName('channel')[0].value = options.channel;
+	if (options && options.message) document.getElementsByName('message')[0].value = options.message;
+	element.classList.add('is-active');
 }
 
-function closeModal($el) {
-	$el.classList.remove('is-active');
-}
-
-function closeAllModals() {
-	(document.querySelectorAll('.modal') || []).forEach(($modal) => {
-		closeModal($modal);
-	});
-}
-
-// Add a click event on buttons to open a specific modal
-(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-	const modal = $trigger.dataset.target;
-	const $target = document.getElementById(modal);
-
-	$trigger.addEventListener('click', () => {
-		openModal($target);
-	});
-});
+function closeModal(element) { element.classList.remove('is-active'); }
 
 // Add a click event on various child elements to close the parent modal
-(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button').forEach($close => {
 	const $target = $close.closest('.modal');
 
 	$close.addEventListener('click', () => {
 		closeModal($target);
 	});
-});
-
-// Add a keyboard event to close all modals
-document.addEventListener('keydown', (event) => {
-	const e = event || window.event;
-
-	if (e.keyCode === 27) closeAllModals();
 });
