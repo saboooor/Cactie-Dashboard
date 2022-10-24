9bbf42b5d29e4a07899dc0a01db719f4f8ea587b
saboooor <saboor547@gmail.com>
2022-10-23 20:37:20 -0600

    show user data in dashboard

--- src/auth.tsx
+++ src/auth.tsx
@@ -1,10 +1,12 @@
 export default function getAuth(request: any) {
     const cookieJSON: any = {};
-    const cookiesArray = request.headers.get('cookie').split(';');
-    cookiesArray.forEach((cookie: string) => {
-        const values = cookie.split('=');
-        cookieJSON[values[0]] = values[1];
-    });
+    if (request.headers.get('cookie')) {
+        const cookiesArray = request.headers.get('cookie').split(';');
+        cookiesArray.forEach((cookie: string) => {
+            const values = cookie.split('=');
+            cookieJSON[values[0]] = values[1];
+        });    
+    }
     const sid = cookieJSON['connect.sid'];
     return sessions[sid];
 }
--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -1,8 +1,13 @@
-import { component$ } from '@builder.io/qwik';
+import { Resource, component$ } from '@builder.io/qwik';
 import type { DocumentHead, RequestHandler } from '@builder.io/qwik-city';
+import { useEndpoint } from "@builder.io/qwik-city";
 import getAuth from '../../auth';
 
-export const onGet: RequestHandler = async ({ url, params, request, response }) => {
+interface ProductData {
+  username: string,
+  discriminator: string
+};
+export const onGet: RequestHandler<ProductData> = async ({ url, params, request, response }) => {
   const auth = getAuth(request);
   if (!auth) throw response.redirect('/login');
   const res = await fetch('https://discord.com/api/users/@me', {
@@ -11,12 +16,23 @@ export const onGet: RequestHandler = async ({ url, params, request, response }) 
     },
   })
   const userdata = await res.json();
-  console.log(userdata);
+  return userdata;
 };
 
 export default component$(() => {
+  const productData = useEndpoint<ProductData>();
   return (
     <section class="mx-auto max-w-6xl px-6 pt-12 items-center" style="height: calc(100vh - 64px);">
+      <Resource
+        value={productData}
+        onPending={() => <div>Loading...</div>}
+        onRejected={() => <div>Error</div>}
+        onResolved={(product) => (
+          <>
+            <h1>{product.username}#{product.discriminator}</h1>
+          </>
+        )}
+      />
       <div class="text-center" style="filter: drop-shadow(0 0 2rem rgba(79, 70, 229, 1));">
         <h1 class="font-bold tracking-tight text-white text-5xl">
           Select a <span class="text-blue-400">Server</span>.
