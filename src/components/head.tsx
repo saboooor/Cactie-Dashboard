09b0bc8e45b711ed87fa77454e6f818a38f4a9b4
saboooor <saboor547@gmail.com>
2022-10-24 23:13:07 -0600

    a

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -18,6 +18,13 @@ export const RouterHead = component$(() => {
       <meta content={head.title} property="og:title" />
       <meta content={`/assets/images/${client.user.username.replace(/ /g, '')}.webp`} property="og:image" />
 
+      <meta content="Embed Title" property="og:title" />
+      <meta content="Site Description" property="og:description" />
+      <meta content="https://embed.com/this-is-the-site-url" property="og:url" />
+      <meta content="https://embed.com/embedimage.png" property="og:image" />
+      <meta content="#43B581" data-react-helmet="true" name="theme-color" />
+
+
       {head.meta.map((m) => (
         <meta {...m} />
       ))}
