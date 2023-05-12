ab4375f78a37a6cb4973bc6bb847aff5ce11754a
saboooor <saboor547@gmail.com>
2023-05-11 18:04:43 -0600

    yes

--- src/components/functions/prisma.ts
+++ src/components/functions/prisma.ts
@@ -1,3 +1,4 @@
 import { PrismaClient } from '@prisma/client/edge';
-export const master = new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL } } });
-export const dev = new PrismaClient({ datasources: { db: { url: process.env.DATABASE_URL_DEV } } });
+
+export const master = new PrismaClient({ datasources: { db: { url: import.meta.env.DATABASE_URL ?? process.env.DATABASE_URL } } });
+export const dev = new PrismaClient({ datasources: { db: { url: import.meta.env.DATABASE_URL_DEV ?? process.env.DATABASE_URL_DEV } } });
